e153f92aa74305ce6374413e4894aa1b4f46f1ff
Ваше Имя <you@example.com>
2019-03-16 01:54:00 +0500

    hhhh

--- auth-lib.js
+++ auth-lib.js
@@ -1,7 +1,7 @@
 let allUsers = [
 	{username: "admin", password: "1234", groups: ["admin", "manager", "basic"]},
-	{username: "sobakajozhec", password: "ekh228", groups: ["basic", "manager"]},
-	{username: "patriot007", password: "russiaFTW", groups: ["basic"]}
+	// {username: "sobakajozhec", password: "ekh228", groups: ["basic", "manager"]},
+	// {username: "patriot007", password: "russiaFTW", groups: ["basic"]}
 ];
 
 
@@ -13,40 +13,57 @@ let allGroups = {
     "basic": [allRights[1], allRights[3]]
 };
 
-function UserException(message){
-    return message;
-}
 
-function createUser(username, pass) {
-    let newUser = {};
-    newUser.username = username;
-    newUser.password = pass;
-    newUser.groups = [];
-    allUsers.push(newUser);
-    return newUser.username;
+function createUser(username, password) {
+
+        let newUser = {};
+        newUser.username = username;
+        newUser.password = password;
+        newUser.groups = ["basic"];
+              allUsers.push(newUser);
+                //return newUser.username; //строка
+        return newUser;
+
+
 };
 
 function deleteUser(user) {
-  let index = allUsers.findIndex(x=>x.username===user);
+    //console.log(user);
+  let index = allUsers.findIndex(x=>x.username===user.username);
   if(index !== -1){
-      allUsers.splice(index,1);
+    allUsers.splice(index,1);
   }else{
-      throw new UserException("Ошибка! Пользователь не найден, или был удален ранее"); // это действие бросает исключение!
+      throw new Error ("Ошибка! Пользователь не найден, или был удален ранее");
   }
   return undefined;
 };
 
 function users() {
+    //let usernames=allUsers.map(function (user) {
+       // return user.username;
+    //});
     return allUsers;
+}
+
+function createGroup() {
+    let newGroup="newGroup";
+    allGroups[newGroup]=[allRights[3]]; //какое?
+    return newGroup;
 };
 
-function createGroup(group) {
-    //allGroups - это объект, поэтому с ним нужно работать соответсвенно
-    allGroups[group]=allRights[2];
-    return group;
-};
+function deleteGroup(group) {
+    //console.log(group);
+    if (typeof group=== "string")
+    {if (group in allGroups){
+    delete allGroups[group];}
+    else
+        throw new Error ("error! что то удаленное");
+   }
+   else
+    {
+        throw new Error ("error! плохой аргумент");
+    }
 
-function deleteGroup() {
 };
 
 function groups() {
@@ -56,47 +73,145 @@ function groups() {
     for (let group in allGroups){
         arrGroups.push(group);
     }
+
     return arrGroups;
 };
 
 function addUserToGroup(user, newGroup) {
-    let index = allUsers.findIndex(x=>x.username===user);
-    allUsers[index].groups.push(newGroup);
-    return undefined;
+    //console.log(newGroup);
+  if ((typeof user === "object") && (typeof newGroup=== "string"))
+  {let indexUser = allUsers.findIndex(x=>x.username===user.username);
+  if ((indexUser!==-1))
+  {
+       user.groups.push(newGroup);
+   }else{
+      throw new Error ("error! что то удаленное");
+  }
+  }
+   else {
+      throw new Error ("error! плохой аргумент");
+   }
+
 }
 
 function userGroups(user) {
-    let index = allUsers.findIndex(x=>x.username===user);
-    return allUsers[index].groups;
+    // var index = allUsers.findIndex(x=>x.username===user);
+    // console.log(user);
+    return user.groups;
 };
 
 function removeUserFromGroup(user, group) {
-    let index = allUsers.findIndex(x=>x.username===user);
-    allUsers[index].groups.splice(allUsers[index].groups.indexOf(group),1);
-};
+    if ((typeof user === "object") && (typeof group=== "string"))
+    {let indexUser = allUsers.findIndex(x=>x.username===user.username);
+        if ((indexUser!==-1) && (group in allGroups))
+        {
 
-function createRight(newRight) {
-    allRights.push(newRight);
+            let index=user.groups.indexOf(group);
+
+            if (index!==-1) {
+            user.groups.splice(index, 1);}
+else
+    {throw new Error ("error! у пользователя нет той группы ");
+
+            }
+
+        }else{
+            throw new Error ("error! что то удаленное");
+        }
+    }
+    else {
+        throw new Error ("error! плохой аргумент");
+    }
+}
+
+function createRight() {
+
+    allRights.push("read");
+   let indexRight=allRights.indexOf("read")
+    return allRights[indexRight];
 };
 
 function deleteRight(right) {
+
+    if (typeof right=== "string"){
     let index=allRights.indexOf(right);
-    allRights.splice(index, 1);
-    return allRights.indexOf(right);
+    if (index !== -1)
+        {console.log(right);
+        allRights.splice(index,1);
+        //пробежать по всем группам и поудалять право??????? этого что ли не хватает?
+        }
+    else
+    {
+        throw new Error("Ошибка! что то удаленное")
+    }}
+    else
+    {
+        throw new Error ("error! плохой аргумент")
+    }
+    return undefined;
 };
 
-function groupRights() {};
+function groupRights(group) {
+    // console.log(allGroups[group]);
+
+    return allGroups[group];
+};
 
 function rights() {
     return allRights;
 };
 
 function addRightToGroup(right,group) {
-    allGroups[group].push(right);
+    if ((typeof right=== "string") && (typeof group=== "string"))
+    {
+        let indexRight= allRights.indexOf(right);
+
+        if ((indexRight !== -1)&&(group in allGroups))
+        {
+            allGroups[group].push(allRights[indexRight]);
+        }
+        else
+        {
+            throw new Error("Ошибка! что то удаленное")
+        }
+    }
+    else
+    {
+        throw new Error ("error! плохой аргумент")
+    }
     return undefined;
 };
 
-function removeRightFromGroup() {};
+function removeRightFromGroup(right,group) {
+
+    if ((typeof right=== "string") && (typeof group=== "string"))
+    {
+        let indexRight= allRights.indexOf(right);
+
+        if ((indexRight !== -1)&&(group in allGroups))
+        {
+            let index=allGroups[group].indexOf(right);
+            if(index!==-1)
+            {
+                allGroups[group].splice(index, 1);
+            }
+            else
+            {
+                throw new Error("Ошибка! в группе нет этого права")
+            }
+        }
+        else
+        {
+            throw new Error("Ошибка! что то удаленное")
+        }
+    }
+    else
+    {
+        throw new Error ("error! плохой аргумент")
+    }
+    return undefined;
+
+};
 
 function login(username, password) {};
 
@@ -105,3 +220,6 @@ function currentUser() {};
 function logout() {};
 
 function isAuthorized(user, right) {};
+
+
+//let index=allGroups[group].indexOf(right);
